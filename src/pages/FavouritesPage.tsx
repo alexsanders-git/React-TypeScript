276abb42db7c5606047797fbdf4ac24f4240bf41
Alex Sanders <alex.sanders@ukr.net>
2023-07-06 19:25:54 +0300

    Update FavouritesPage.tsx

--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -7,7 +7,7 @@ const FavouritesPage = () => {
 
 	const { favourites } = useAppSelector(state => state.github)
 
-	if (favourites.length === 0) return <p className="text-center">No items</p>
+	if (favourites.length === 0) return <p className="text-center my-5">No items</p>
 
 	return (
 		<div className="flex justify-center pt-10 mx-auto h-screen w-screen w-full max-w-[560px]">
