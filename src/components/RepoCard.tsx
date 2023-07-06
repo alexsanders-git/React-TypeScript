1707a4b32e9921cc3cc77c31baf5f30c3db2c296
Alex Sanders <alex.sanders@ukr.net>
2023-07-06 19:25:07 +0300

    Added the functionality of saving to favorites

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import { useActions } from "../hooks/actions";
+import { useAppSelector } from "../hooks/redux";
 import { IRepo } from "../models/models";
 
 const RepoCard = ({ repo }: { repo: IRepo }) => {
+
+	const { addFavourite, removeFavourite } = useActions()
+
+	const { favourites } = useAppSelector(state => state.github)
+
+	const [isFav, setIsFav] = useState(favourites.includes(repo.html_url))
+
+	const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault()
+		addFavourite(repo.html_url)
+		setIsFav(true)
+	}
+
+	const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault()
+		removeFavourite(repo.html_url)
+		setIsFav(false)
+	}
+
 	return (
 		<div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
 			<a href={repo.html_url} target="_blank">
@@ -10,6 +32,20 @@ const RepoCard = ({ repo }: { repo: IRepo }) => {
 					Watchers: <span className="font-bold">{repo.watchers}</span>
 				</p>
 				<p className="text-sm font-thin">{repo?.description}</p>
+
+				{!isFav && <button
+					className="py-2 px-4 bg-yellow-400 rounded hover:shadow-sm transition-all"
+					onClick={addToFavourite}
+				>
+					Add
+				</button>}
+
+				{isFav && <button
+					className="py-2 px-4 bg-red-400 rounded hover:shadow-sm transition-all"
+					onClick={removeFromFavourite}
+				>
+					Remove
+				</button>}
 			</a>
 		</div>
 	);
--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,6 +1,37 @@
+import { useActions } from "../hooks/actions";
+import { useAppSelector } from "../hooks/redux";
+
 const FavouritesPage = () => {
+
+	const { removeFavourite } = useActions()
+
+	const { favourites } = useAppSelector(state => state.github)
+
+	if (favourites.length === 0) return <p className="text-center">No items</p>
+
 	return (
-		<h1>FavouritesPage</h1>
+		<div className="flex justify-center pt-10 mx-auto h-screen w-screen w-full max-w-[560px]">
+			<ul className="list-none w-full">
+				{favourites.map(f => (
+					<li key={f} className="flex justify-between items-center gap-5 p-2 m-1 border">
+						<a
+							href={f}
+							target="_blank"
+							className="hover:text-blue-800 transition-colors"
+						>
+							{f}
+						</a>
+
+						<button
+							className="py-2 px-4 bg-red-400 rounded hover:shadow-sm transition-all"
+							onClick={() => removeFavourite(f)}
+						>
+							Remove
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
 
